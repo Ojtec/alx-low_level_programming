501ad2e0f8fad553a13a53610e8771cc35a54f94
Ojtec <ajayifatai1434@yahoo.com>
2022-09-14 19:51:51 -0700

    insomnia

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#inlude "main.h"
+/**
+ * main - Prints lowercase
+ *
+ * Return: output(Okay)
+ */
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
+
