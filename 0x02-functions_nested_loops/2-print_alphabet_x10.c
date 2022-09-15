c3431ff96c3396f8ff717eb7bf07aeaaf946701a
Ojtec <ajayifatai1434@yahoo.com>
2022-09-14 20:32:32 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - function to print lowerase
+ *
+ * Return: Output(okay)
+ */
+void print_alphabet_x10(void)
+{
+	char c, i;
+
+	for (i = 0; i <= 0; i++)
+	{
+		for(c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+	}
+}
+
