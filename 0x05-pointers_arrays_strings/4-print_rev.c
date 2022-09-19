05e540dd59ad24898980f5ca6c653e8c05822ca7
Ojtec <ajayifatai1434@yahoo.com>
2022-09-19 16:07:31 -0700

    I can only go one way

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * print_rev - reverse order printing
+ *
+ * @s: string to reverse
+ * Return: 0
+ */
+
+void print_rev(char *s)
+{
+	int len = strlen(s)
+
+		while (len--)
+			putchar(*(s + len));
+	putchar(10);
+}
+
