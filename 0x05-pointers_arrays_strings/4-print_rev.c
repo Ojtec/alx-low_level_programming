4902e8726b728f082020504e0052d4f544eabb41
Ojtec <ajayifatai1434@yahoo.com>
2022-09-20 15:20:08 -0700

    print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -10,10 +10,13 @@
 
 void print_rev(char *s)
 {
-	int len = strlen(s)
+	int i, n;
 
-		while (len--)
-			putchar(*(s + len));
-	putchar(10);
+	s = "hello"
+		n = strlen(s);
+	for (i = n - 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
 }
 
